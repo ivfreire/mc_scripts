7c3187710eba4739bf6cba61dedbbf304022ac76
Ícaro Freire <icarovf@usp.br>
2020-11-12 03:46:00 -0300

    new mc_finder.py

--- mc_finder.py
+++ mc_finder.py
@@ -1,10 +1,1 @@
-'''
-	Created by Ícaro Freire in November 2020.
-	São Paulo, BR.
 
-	
-	OBS: I am afraid this script will only work out with cracked servers, since
-	communication between original servers and clients is encrypted.
-'''
-
-import socket, sys, json
